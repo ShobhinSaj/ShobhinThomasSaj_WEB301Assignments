4d94f90365d4f67840ed2e65adae44b77d1dda39
madmax117 <maxjonas117@gmail.com>
2023-11-06 13:11:25 -0500

    Commit

--- js/script.js
+++ js/script.js
@@ -20,5 +20,37 @@ $(document).ready(function(){
         scrollbar: {
           el: '.swiper-scrollbar',
         },
-      });  
+      });
+      
+      // Form validation using Validate.js plugin
+        $("#payment").validate({
+          rules: {
+            cardnum: {
+              required: true,
+              // 
+              number:true,
+              minlength:10,
+              maxlength:10
+            },
+            cvv: {
+              required: true,
+              // pattern: /^[0-9]{3}$/
+              number:true,
+              minlength:3,
+              maxlength:3 
+            }
+          },
+          messages: {
+            cardnum: {
+              required: "Please enter a 10-digit credit card number.",
+              pattern: "Please enter a valid 10-digit credit card number."
+            },
+            cvv: {
+              required: "Please enter a 3-digit CVV.",
+              pattern: "Please enter a valid 3-digit CVV."
+            }
+          }
+         
+        });
+      
 });
